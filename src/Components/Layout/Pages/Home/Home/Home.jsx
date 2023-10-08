dc83973520847bedea22992b66171b81e5216106
Ritupriya <ritupriyabgc@gmail.com>
2023-10-08 11:28:39 +0600

    home page complete

--- src/Components/Layout/Pages/Home/Home/Home.jsx
+++ src/Components/Layout/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import Reviewers from "../Reviewers/Reviewers";
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             </Helmet>
             <Banner></Banner>
             <Category></Category>
+             <Reviewers></Reviewers>
         </div>
     );
 };
