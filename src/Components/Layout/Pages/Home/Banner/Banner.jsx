6f2b4ecefcdd0f46ef5c41bbf7e272d7b13bf258
Ritupriya <ritupriyabgc@gmail.com>
2023-10-08 08:02:00 +0600

    Banner Added

---
+++ src/Components/Layout/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,45 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import Img1 from '../../../../../assets/Images/Banner/1.png'
+import Img2 from '../../../../../assets/Images/Banner/2(1).jpg'
+import Img3 from '../../../../../assets/Images/Banner/3.jpg'
+import Img4 from '../../../../../assets/Images/Banner/1.jpg'
+
+
+
+
+const Banner = () => {
+    return (
+        <div className=" md:pt-24 md:pb-4">
+        <Carousel>
+        
+        {/* className="w-10/12 rounded-lg shadow-2xl mt-20 md:mt-0 mx-auto" */}
+       
+            <div >
+              <img src={Img1}  />
+              
+            </div>
+         
+               
+            <div >
+              <img src={Img2}  />
+              
+            </div>
+         
+            <div >
+              <img src={Img3}  />
+              
+            </div>
+            <div >
+              <img src={Img4}  />
+              
+            </div>
+            
+         
+            </Carousel>
+      </div>
+    );
+};
+
+export default Banner;
--- src/Components/Layout/Pages/Shear/Navbar.jsx
+++ src/Components/Layout/Pages/Shear/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
         <>
              <div>
             {/*  */}
-            <div className="navbar bg-transparent px-4 py-2 fixed z-10 ">
+            <div className="navbar bg-orange-200 px-4 py-2 fixed z-10 ">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <label tabIndex={0} className="btn btn-outline btn-error mr-4 lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow  bg-orange-300 text-orange-900 font-bold rounded-box w-52 ">
@@ -38,7 +38,7 @@ const Navbar = () => {
                     </div>
                 </div>
                 <div className="flex-none">
-                    <div className="dropdown dropdown-end text-orange-900 navbar-center hidden lg:flex">
+                    <div className="dropdown dropdown-end text-orange-700 navbar-center hidden lg:flex">
 
                         <div className='font-bold text-lg mr-2'>
                         <Link to='/' className='mr-4'>Home</Link>
@@ -68,7 +68,7 @@ const Navbar = () => {
                             <button className='w-16' onClick={handleLogout}>Logout</button>
                         </ul>
                     </div> : */}
-                             <div className='font-bold text-lg ml-2 text-orange-900'>Login
+                             <div className='font-bold text-lg ml-2 text-orange-700'>Login
                                </div>
                     
                    
